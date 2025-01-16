7de4fcb20b5bffa2b3a65e7698d13df9a08b2009
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-01-16 21:26:56 +0530

    api using flask code added

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask;
 
-app = Flask(__name__)
+app = Flask(__name__);
 
 @app.route("/info")
 def test():
-    return "Hello, Welcome to ..."
+    return "Hello, Welcome to ...";
 
 app.run(host='0.0.0.0');
